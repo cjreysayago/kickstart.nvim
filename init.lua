2803dbd1dd56fcc559c7cfe2eb6ad9df991dbb1b
cjreysayago <crey@purrfectlabs.com>
2024-08-16 19:18:59 -0300

    add spell check feature

--- init.lua
+++ init.lua
@@ -161,6 +161,9 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+vim.opt.spelllang = 'en_us'
+vim.opt.spell = true
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
